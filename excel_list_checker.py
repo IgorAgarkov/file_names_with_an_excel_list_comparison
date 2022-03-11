d9927b639bdf4a0e257a2d96b2e07e5e27677e9c
IgorAgarkov <agarkov.ig@mail.ru>
2022-03-11 08:25:30 +0700

    combined functions of four scripts in one

---
+++ excel_list_checker.py
@@ -0,0 +1,46 @@
+# -*- coding: utf8 -*-
+
+from openpyxl import load_workbook
+import os
+
+excel_link = r'D:\tmp\Новая папка (4)\Заявка №хх от 00.00.2021.xlsx'   # ссылка на файл excel
+lile_link  = r'.'                                    # ссылка на папку с файлами
+ext    = 'dxf' # расшерение файла
+column = 'B'   # столбец в excel
+cell   = 6     # номер начальной ячейки в excel
+
+files = os.listdir(lile_link)   # получаем список всех файлов в директории 
+no_ext_files = []
+
+# наполняем список no_ext_files названиями файлов dxf без расширений
+for i in files:
+    if i[-4:].lower() == '.' + ext:
+        no_ext_files.append(i[:-(len(ext) + 1)])  # 
+wb = load_workbook(excel_link)           
+ws = wb.active
+
+excel_list = []
+
+# наполняем список excel_list значениями ячеек
+while ws[column + str(cell)].value != None:
+    excel_list.append(ws[column + str(cell)].value)
+    cell += 1
+
+flag = True
+
+# сравниваем каждый файл со значением ячеек столбца B
+for dxf in no_ext_files:          # перебираем все файлы
+    if dxf not in excel_list:
+        print('Нет записи:', dxf)
+        flag = False
+
+print()
+
+# сравниваем каждую ячейку (из диапазона) столбца B со списком файлов no_ext_files
+for exl in excel_list:          # диапазон строк в файле Excel
+    if exl not in no_ext_files:
+        print('Нет файла:', exl)
+        flag = False
+
+if flag == True:
+    print('Всё в порядке! Список позиций Excel и файлы DXF соответствуют друг другу.')
